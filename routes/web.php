c8f6cc787e7462ee9a095cc6c08022ffc97ebabf
MesbaHossein2010 <s.mesbahossein@gmail.com>
2025-02-11 21:13:04 +0330

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::group(['prefix' => 'cal'], function () {
+
+    Route::get("/", function () {
+        return '<center style="font-size: 200px;"><a href="http://localhost:8000/">GO HOME</a></center>';
+    });
+
+    Route::get('/X/{num}/{num2}', function ($num, $num2) {
+        $var = $num * $num2;
+        return view("cal", compact('var'));
+    })->where(['num', 'num2'], "[0-9]+");
+
+    Route::get('/+2/{num}', function ($num) {
+        $var = $num + 2;
+        return view("cal", compact('var'));
+    })->where('num', "[0-9]+");
+
+});
+
+Route::fallback(function () {
+    return "<center style='font-size: 200px; color: red' >No domain like this <b style='color: #000000' >dummy</b></center>";
+});
